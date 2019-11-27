ee8b6765d2a68c8d6434364701af0f7759d38f48
Logan Smith <logsmith06@gmail.com>
2019-11-27 02:13:10 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
 class Backer
-  attr_reader :backed_projects
-
-  def initialize(backed_projects = [])
-    @backed_projects = backed_projects
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @backed_projects = []
   end
 
   def back_project()
